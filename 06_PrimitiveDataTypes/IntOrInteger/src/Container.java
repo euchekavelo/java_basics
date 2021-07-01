28a72b3fda5e7d7c79a23097ae2fa5f672d31b0b
Skillbox <it@skillbox.ru>
2021-07-01 16:26:32 +0000

    Add lesson

---
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Container.java
@@ -0,0 +1,11 @@
+public class Container {
+    private Integer count;
+
+    public void addCount(int value) {
+        count = count + value;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
---
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Container container = new Container();
+        container.addCount(5672);
+        System.out.println(container.getCount());
+
+        // TODO: ниже напишите код для выполнения задания:
+        //  С помощью цикла и преобразования чисел в символы найдите все коды
+        //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
+
+    }
+}
