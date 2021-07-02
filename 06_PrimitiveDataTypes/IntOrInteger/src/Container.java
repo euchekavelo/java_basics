bc3dce26145e84cebf5a8af2f44ef2adaf470e50
Kirill <39271743+euchekavelo@users.noreply.github.com>
2021-07-02 23:28:58 +0300

    Completed task 6.9 (modified files: Container.java, Main.java)

--- 06_PrimitiveDataTypes/IntOrInteger/src/Container.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Container.java
@@ -1,5 +1,5 @@
 public class Container {
-    private Integer count;
+    private Integer count = 0;
 
     public void addCount(int value) {
         count = count + value;
--- 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
@@ -8,5 +8,12 @@ public class Main {
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
 
+
+        for (int i = 0; i < 65536; i++)
+        {
+            char symbol = (char) i;
+            if ((symbol >= 'а' && symbol <= 'я') || (symbol >= 'А' && symbol <='Я') || (symbol == 'ё') || (symbol == 'Ё'))
+                System.out.println("Код Unicode " + i + " соответствует символу " + symbol);
+        }
     }
 }
