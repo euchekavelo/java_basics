b5c6dedf0741a3c2b6438ca21b59e3ce53a76846
Kirill <39271743+euchekavelo@users.noreply.github.com>
2021-07-07 16:37:32 +0300

    Completed task 7.10 (modified directory files: Date and Periods)

--- 07_NumbersAndDates/Date/src/main/java/Main.java
+++ 07_NumbersAndDates/Date/src/main/java/Main.java
@@ -1,3 +1,9 @@
+import javax.swing.text.DateFormatter;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 public class Main {
 
@@ -16,7 +22,31 @@ public class Main {
         //TODO реализуйте метод для построения строки в следующем виде
         //0 - 31.12.1990 - Mon
         //1 - 31.12.1991 - Tue
-        
-        return "";
+
+        LocalDate birthday = LocalDate.of(year, month, day); //Дата рождения
+        LocalDate today = LocalDate.now(); //Текущая дата
+
+        if (birthday.compareTo(today) > 0)
+            return "";
+        else
+        {
+            int currentAge = 0; //Начальное значение текущего возраста
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy"); //Зададим формат для вывода даты
+            DayOfWeek dayOfWeek = birthday.getDayOfWeek(); //Получим день недели первого дня рождения (пример. "FRIDAY").
+            String shortDayOfWeek = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.ENGLISH); //Преобразуем полученный день недели в краткий формат "Fri"
+
+            //Сформируем первую запись в требуемом формате вывода
+            String outputLine = currentAge + " - " + formatter.format(birthday) + " - " + shortDayOfWeek + System.lineSeparator();
+            birthday = birthday.plusYears(1); //Прибавим год
+
+            while (today.isAfter(birthday) || birthday.compareTo(today) == 0)
+            {
+                dayOfWeek = birthday.getDayOfWeek();
+                shortDayOfWeek = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.ENGLISH);
+                outputLine = outputLine + ++currentAge + " - " + formatter.format(birthday) + " - " + shortDayOfWeek + System.lineSeparator();
+                birthday = birthday.plusYears(1);
+            }
+            return outputLine;
+        }
     }
 }
--- 07_NumbersAndDates/Periods/src/main/java/Main.java
+++ 07_NumbersAndDates/Periods/src/main/java/Main.java
@@ -1,13 +1,31 @@
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args)
+  {
+
+    int day = 21;
+    int month = 1;
+    int year = 1995;
+
+    LocalDate birthday = LocalDate.of(year, month, day);
+    System.out.println(getPeriodFromBirthday(birthday));
 
   }
 
-  private static String getPeriodFromBirthday(LocalDate birthday) {
-    return "";
+  private static String getPeriodFromBirthday(LocalDate birthday)
+  {
+
+    LocalDate today = LocalDate.now(); //Получим текущую дату
+    Period period = Period.between(birthday, today); //Получим число лет, месяцев и дней между двумя датами (разница)
+    int years = period.getYears(); //Получим число лет
+    int months = period.getMonths(); //Получим число месяцев
+    int days = period.getDays(); //Получим число дней
+
+    return years + " years, " + months + " months, " + days + " days ";
+
   }
 
 }
