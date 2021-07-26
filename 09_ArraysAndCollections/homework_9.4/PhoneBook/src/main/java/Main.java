6eb19a0e0fb0ce5d911c697e5bf2eb774ff693f3
Kirill <39271743+euchekavelo@users.noreply.github.com>
2021-07-26 19:10:34 +0300

    Completed task 9.4 (modifie files: Main.java, PhoneBook.java; added file: InputUser.java)

--- 09_ArraysAndCollections/homework_9.4/PhoneBook/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.4/PhoneBook/src/main/java/Main.java
@@ -1,5 +1,56 @@
+import java.util.Set;
+
 public class Main {
+
+    private final static String DIGITAL_INPUT = "\\p{Digit}+";
+    private final static String ALPHABET_INPUT = "[а-яА-Я]+";
+
     public static void main(String[] args) {
+
+        PhoneBook phoneBook = new PhoneBook();
+
+        while (true)
+        {
+            System.out.println("Введите номер, имя или команду:");
+            String inputUser = InputUser.getLine();
+            if (inputUser.equals("LIST"))
+            {
+                phoneBook.list();
+            }
+            else if (inputUser.matches(DIGITAL_INPUT))
+            {
+                String foundPhone = phoneBook.getNameByPhone(inputUser);
+                if (foundPhone.isEmpty())
+                {
+                    System.out.println("Такого номера нет в телефонной книге.\n" +
+                            "Введите имя абонента для номера \"" + inputUser + "\":");
+                    String inputName = InputUser.getLine();
+                    phoneBook.addContact(inputUser, inputName);
+                }
+                else
+                    System.out.println(foundPhone);
+            }
+            else if (inputUser.matches(ALPHABET_INPUT))
+            {
+                Set<String> subscribers = phoneBook.getPhonesByName(inputUser);
+                if (subscribers.isEmpty())
+                {
+                    System.out.println("Такого имени в телефонной книге нет.\n" +
+                            "Введите номер телефона для абонента \"" + inputUser + "\":");
+                    String inputTelephone = InputUser.getLine();
+                    phoneBook.addContact(inputTelephone, inputUser);
+                }
+                else
+                {
+                    for (String element : subscribers)
+                    {
+                        System.out.println(element);
+                    }
+                }
+            }
+            else
+                System.out.println("Неверный формат ввода");
+        }
 
     }
 }
--- 09_ArraysAndCollections/homework_9.4/PhoneBook/src/main/java/PhoneBook.java
+++ 09_ArraysAndCollections/homework_9.4/PhoneBook/src/main/java/PhoneBook.java
@@ -1,29 +1,98 @@
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class PhoneBook {
+
+    private Map<String, String> phoneBook = new TreeMap<>();
+    private final static String REGEX_NAME = "[А-Я][а-я]+";
+    private final static String REGEX_PHONE_NUMBER = "79\\p{Digit}{9}";
 
     public void addContact(String phone, String name) {
         // проверьте корректность формата имени и телефона
         // если такой номер уже есть в списке, то перезаписать имя абонента
+
+        if (name.matches(REGEX_NAME) && phone.matches(REGEX_PHONE_NUMBER))
+        {
+            phoneBook.put(phone, name);
+            System.out.println("Контакт сохранен!");
+        }
+        else
+            System.out.println("Неверный формат ввода имени или телефона.\n" +
+                    "Имя абонента должно состоять минимум из 2-ух букв русского алфавита, где 1-ая буква - заглавная, 2-ая - строчная.\n" +
+                    "Номер телефона должен состоять из 11 цифр, где первые два числа 7 и 9.");
     }
 
     public String getNameByPhone(String phone) {
         // формат одного контакта "Имя - Телефон"
         // если контакт не найдены - вернуть пустую строку
-        return "";
+
+        if (phoneBook.get(phone) == null)
+            return "";
+        else
+            return phoneBook.get(phone) + " - " + phone;
     }
 
     public Set<String> getPhonesByName(String name) {
         // формат одного контакта "Имя - Телефон"
         // если контакт не найден - вернуть пустой TreeSet
-        return new TreeSet<>();
+
+        Set<String> foundContacts = new TreeSet<>();
+        for (Map.Entry<String, String> element : phoneBook.entrySet())
+        {
+            String currentKey = element.getKey();
+            String currentValue = element.getValue();
+            if (currentValue.equals(name))
+            {
+                foundContacts.add(currentValue + " - " + currentKey);
+            }
+        }
+
+        return foundContacts;
     }
 
     public Set<String> getAllContacts() {
         // формат одного контакта "Имя - Телефон"
         // если контактов нет в телефонной книге - вернуть пустой TreeSet
-        return new TreeSet<>();
+
+        Set<String> printAllContacts = new TreeSet<>();
+        for (String key : phoneBook.keySet())
+        {
+            printAllContacts.add(phoneBook.get(key) + " - " + key);
+        }
+
+        return printAllContacts;
     }
 
+    public void list()
+    {
+        Map <String, String> newMap = new TreeMap<>();
+        HashSet <String> uniqueValue = new HashSet(phoneBook.values());
+        for (String element : uniqueValue)
+        {
+            //Зададим индикатор, который будет учитываться в проверки найденного дубля значения.
+            boolean initialFinding = false;
+            for (Map.Entry<String, String> entry : phoneBook.entrySet())
+            {
+                String currentKey = entry.getKey();
+                String currentValue = entry.getValue();
+                if (element.equals(currentValue) && !initialFinding)
+                {
+                    newMap.put(element, currentKey);
+                    initialFinding = true;
+                }
+                else if (element.equals(currentValue) && initialFinding)
+                {
+                    String currentValueNewMap = newMap.get(element);
+                    newMap.put(element, currentValueNewMap + ", " + currentKey);
+                }
+            }
+        }
+
+        String outLine = "";
+        for (String key : newMap.keySet())
+        {
+            outLine = outLine.concat(key + " - " + newMap.get(key) + "\n");
+        }
+
+        System.out.println(outLine.trim());
+    }
 }
