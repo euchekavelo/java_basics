20ad64624305e428b48b00f065026d9c971fd124
Kirill <39271743+euchekavelo@users.noreply.github.com>
2021-08-11 18:19:42 +0300

    Completed task 11.2 (modified projects: Airport, Employees)

--- 11_AdvancedOOPFeatures/homework_11.2/Airport/src/main/java/Main.java
+++ 11_AdvancedOOPFeatures/homework_11.2/Airport/src/main/java/Main.java
@@ -1,17 +1,45 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
+import com.skillbox.airport.Terminal;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
+        Airport airport = Airport.getInstance();
+        findPlanesLeavingInTheNextTwoHours(airport).forEach(System.out::println);
 
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+
+        //Получим текущее время.
+        LocalDateTime currentLocalDateTime = LocalDateTime.now();
+
+        //Переменная для хранения текущего времени, увеличенного на два часа.
+        LocalDateTime currentTimeTwoHours = currentLocalDateTime.plusHours(2L);
+
+        List<Flight> flights= new ArrayList<>();
+        for (Terminal terminal : airport.getTerminals()) {
+            terminal.getFlights().stream()
+                    .filter(flight -> currentLocalDateTime.compareTo(getLocalDateTimeFromDate(flight.getDate())) <= 0
+                            && currentTimeTwoHours.compareTo(getLocalDateTimeFromDate(flight.getDate())) >= 0
+                            && flight.getType() == Flight.Type.DEPARTURE)
+                    .forEach(flights::add);
+        }
+
+        return flights;
+    }
+
+    //Метод для преобразования даты в тип LocalDateTime.
+    public static LocalDateTime getLocalDateTimeFromDate(Date date)
+    {
+        return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
     }
 
 }
--- 11_AdvancedOOPFeatures/homework_11.2/Employees/src/main/java/Main.java
+++ 11_AdvancedOOPFeatures/homework_11.2/Employees/src/main/java/Main.java
@@ -1,4 +1,10 @@
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -10,9 +16,19 @@ public class Main {
         System.out.println(employeeMaxSalary);
     }
 
-    public static Employee findEmployeeWithHighestSalary(List<Employee> staff, int year) {
-        //TODO Метод должен вернуть сотрудника с максимальной зарплатой среди тех,
-        // кто пришёл в году, указанном в переменной year
-        return null;
+    public static Employee findEmployeeWithHighestSalary(List<Employee> staff, int year)
+    {
+        Stream<Employee> stream = staff.stream();
+        Optional<Employee> optional = stream.filter(employee -> getLocalDateYear(employee.getWorkStart()) == year)
+                                            .max(Comparator.comparing(Employee::getSalary));
+
+        return optional.orElse(null);
+    }
+
+    //Метод для получения значения года из типа LocalDate.
+    public static int getLocalDateYear(Date date)
+    {
+        LocalDate localDate = LocalDate.ofInstant(date.toInstant(), ZoneId.systemDefault());
+        return localDate.getYear();
     }
 }
