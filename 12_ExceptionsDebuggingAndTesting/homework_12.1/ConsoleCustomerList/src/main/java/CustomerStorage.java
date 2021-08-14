4fffa446eb5f6aa3840c3df1d2c3b5b7a2d702b6
Kirill <39271743+euchekavelo@users.noreply.github.com>
2021-08-14 17:35:24 +0300

    Completed task 12.1 (modified files: CustomerStorage.java, Main.java)

--- 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -3,6 +3,8 @@ import java.util.Map;
 
 public class CustomerStorage {
     private final Map<String, Customer> storage;
+    private final static String REGEX_PHONE = "(\\+?7|8)9\\p{Digit}{9}";
+    private final static String REGEX_EMAIL = "\\p{Alpha}\\p{Alnum}*@\\p{Alpha}+\\.(\\p{Alpha}){2,}";
 
     public CustomerStorage() {
         storage = new HashMap<>();
@@ -15,8 +17,17 @@ public class CustomerStorage {
         final int INDEX_PHONE = 3;
 
         String[] components = data.split("\\s+");
-        String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
-        storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
+        if (components.length == 4) {
+            if (!components[INDEX_PHONE].matches(REGEX_PHONE))
+                throw new IllegalArgumentException("Задано некорректное значение номера телефона.");
+            if (!components[INDEX_EMAIL].matches(REGEX_EMAIL))
+                throw new IllegalArgumentException("Задано некорректное значение электронной почты.");
+
+            String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
+            storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
+        }
+        else
+            throw new IllegalArgumentException("Получено неверное количество компонентов в переданной строке.");
     }
 
     public void listCustomers() {
--- 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/Main.java
+++ 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/Main.java
@@ -13,16 +13,24 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         CustomerStorage executor = new CustomerStorage();
 
-        while (true) {
+       while (true) {
             String command = scanner.nextLine();
             String[] tokens = command.split("\\s+", 2);
 
             if (tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
+                try {
+                    executor.addCustomer(tokens[1]);
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                }
             } else if (tokens[0].equals("list")) {
                 executor.listCustomers();
-            } else if (tokens[0].equals("remove")) {
-                executor.removeCustomer(tokens[1]);
+            } else if (tokens[0].equals("remove")) { //Команда может выбросить исключение ArrayIndexOutOfBoundsException
+                try {
+                    executor.removeCustomer(tokens[1]);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println(e.getMessage());
+                }
             } else if (tokens[0].equals("count")) {
                 System.out.println("There are " + executor.getCount() + " customers");
             } else if (tokens[0].equals("help")) {
@@ -30,6 +38,7 @@ public class Main {
             } else {
                 System.out.println(COMMAND_ERROR);
             }
-        }
+       }
+
     }
 }
