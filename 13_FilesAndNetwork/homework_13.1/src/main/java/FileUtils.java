eb2c440ef328776d9280bbb5ae1f70160a2c8904
Kirill <39271743+euchekavelo@users.noreply.github.com>
2021-08-23 05:05:25 +0300

    Completed task 13.1 (modified files: Main.java, FileUtils.java)

--- 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
@@ -1,6 +1,24 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        long totalSize = 0;
+        Path absolutePath = Paths.get(path);
+        try {
+            totalSize = Files.walk(absolutePath)
+                             .map(Path::toFile)
+                             .filter(File::isFile)
+                             .mapToLong(File::length)
+                             .sum();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return totalSize;
     }
+
 }
--- 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        for (; ; ) {
+            System.out.println("Укажите путь до папки:");
+            String line = scanner.nextLine().trim();
+            System.out.println("Размер запрашиваемой папки равен: " + FileUtils.calculateFolderSize(line) + " byte.");
+        }
 
     }
 }
