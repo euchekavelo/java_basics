1dd91c412e7914c79b332808ef3bedc51d5ea0de
Kirill <39271743+euchekavelo@users.noreply.github.com>
2021-08-23 20:37:00 +0300

    Completed task 13.2 (modified files: FileUtils.java, Main.java)

--- 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
@@ -1,5 +1,27 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 public class FileUtils {
+
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+        Path whereToGet = Paths.get(sourceDirectory);
+        Path whereToInsert = Paths.get(destinationDirectory);
+        try {
+            Files.walk(whereToGet)
+                    .forEach(p-> copyContent(p, whereToInsert.resolve(whereToGet.relativize(p))));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    private static void copyContent(Path sourcesDirectoryPaths, Path destinationDirectoryPaths) {
+        try {
+            Files.copy(sourcesDirectoryPaths, destinationDirectoryPaths, StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for(; ;){
+            System.out.println("Введите адрес папки, которую необходимо скопировать: ");
+            String sourceDirectory = scanner.nextLine().trim();
+            System.out.println("Введите адрес директории, в которую необходимо скопировать папку: ");
+            String destinationDirectory = scanner.nextLine().trim();
+            FileUtils.copyFolder(sourceDirectory, destinationDirectory);
+        }
     }
+
 }
