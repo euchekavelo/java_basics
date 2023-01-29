225658b5bb5539cfe3520f79ec802a39574dd000
Skillbox <it@skillbox.ru>
2023-01-29 13:41:59 +0000

    Add lesson

---
+++ Multithreading/Transactions/src/Account.java
@@ -0,0 +1,21 @@
+public class Account {
+
+    private long money;
+    private String accNumber;
+
+    public long getMoney() {
+        return money;
+    }
+
+    public void setMoney(long money) {
+        this.money = money;
+    }
+
+    public String getAccNumber() {
+        return accNumber;
+    }
+
+    public void setAccNumber(String accNumber) {
+        this.accNumber = accNumber;
+    }
+}
